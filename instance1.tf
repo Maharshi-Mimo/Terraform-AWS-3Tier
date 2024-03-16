b9f738bdab15316442277b2c7cd273031a27a730
Maharshi <maharshid32@gmail.com>
2024-03-17 03:44:50 +0530

    created instance for nginx

---
+++ instance1.tf
@@ -0,0 +1,13 @@
+resource "aws_instance" "nginx" {
+  ami                         = "ami-03bb6d83c60fc5f7c"
+  instance_type               = "t2.micro"
+  key_name                    = "K8sproject"
+  monitoring                  = true
+  vpc_security_group_ids      = ["${aws_security_group.sg.id}"]
+  subnet_id                   = aws_subnet.public_subnet.id
+  associate_public_ip_address = true
+  tags = {
+    Name = "terraform-nginx"
+  }
+  user_data = file("user_data_ins1.sh")
+}
